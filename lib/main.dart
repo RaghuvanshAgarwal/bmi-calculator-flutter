d4ae8cc7e7d2a7d55568865c1f588c96d37ffe4b
Raghuvansh Agarwal <rarchva@gmail.com>
2026-06-21 23:56:17 +0530

    Project Init

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,30 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        appBarTheme: AppBarThemeData(
+          backgroundColor: Color(0xFF0A0E21),
+          foregroundColor: Colors.white,
+          elevation: 20,
+          shadowColor: Colors.black87,
+          surfaceTintColor: Colors.transparent,
+        ),
+        scaffoldBackgroundColor: Color(0xFF0C1234),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: Colors.white, fontFamily: 'Nunito'),
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.w800,
+            fontSize: 20,
+            fontFamily: 'SNPro',
+          ),
+        ),
+      ),
       home: InputPage(),
     );
   }
 }
+//0A0E21
 
 class InputPage extends StatefulWidget {
   @override
@@ -20,13 +40,10 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
+      appBar: AppBar(title: Text('BMI CALCULATOR')),
+      body: Center(child: Text('Body Text')),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: Icon(Icons.add),
       ),
     );
